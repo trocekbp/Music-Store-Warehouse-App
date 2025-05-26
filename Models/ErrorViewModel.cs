13829eb7d43744f8a471d8414e91b8721c0020bf
Sadmin <Sadmin@DESKTOP-FKNHS8I>
2025-05-26 22:45:16 +0200

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Music_Store_Warehouse_App.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
