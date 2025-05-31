49538289ee8ac541803acbc220f063bf4793ed63
Kacper T <trocevicz@gmail.com>
2025-05-31 23:45:44 +0200

    Home page improvments.

--- Program.cs
+++ Program.cs
@@ -39,7 +39,7 @@ app.MapStaticAssets();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Instruments}/{action=Index}/{id?}")
+    pattern: "{controller=Home}/{action=Index}/{id?}")
     .WithStaticAssets();
 
 
